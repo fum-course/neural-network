7917e8c5a725cc86124f1fa2a74f9b23bdf2b701
Mohsen Gholami <imohsen2002@gmail.com>
2025-03-08 12:51:10 +0330

    Update header.js

--- data/header.js
+++ data/header.js
@@ -5,5 +5,5 @@ export const header = {
     '<span class="text-red-light">F</span>erdowsi <span class="text-red-light">U</span>niversity Of <span class="text-red-light">M</span>ashhad',
   course: '<span class="text-red-light">Course</span> Name',
   background:
-    "https://s3.amazonaws.com/hoth.bizango/images/797280/cloud-computing-msc_feature.jpg",
+    "https://wallpaperbat.com/img/289369-neural-networks-the-theoretical-understanding-data-scientist.jpg",
 };
