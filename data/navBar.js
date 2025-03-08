a49771ba4a33af25d1b4feb25eef8ae12e639a34
Mohsen Gholami <imohsen2002@gmail.com>
2025-03-08 15:21:52 +0330

    Update navBar.js

--- data/navBar.js
+++ data/navBar.js
@@ -1,5 +1,5 @@
 export const navBar = [
-    { text: "FUMCLOUD", link: "#" },
+    { text: "FUMNeoron", link: "#" },
     { text: "Overview", link: "#Overview" },
     { text: "Content", link: "#Content" },
     { text: "Team", link: "#Team" },
