9358e6f047c6b447d1b320843a2a6ef8c0b46f13
Mohsen Gholami <imohsen2002@gmail.com>
2025-03-08 15:15:29 +0330

    Update lectures.js

--- data/lectures.js
+++ data/lectures.js
@@ -1,83 +1,107 @@
 export const lectures = [
-  {
-    text: "Introduction to Course",
+   {
+    text: "Inroduction to neural network",
     requirements: "You and your motivation.", // optional
-    startText: "Lets start to know what is the course and how it improve...", // optional
+    startText: "Lets start to know what is the nueral network and deep learning...", // optional
     sections: [
       {
         text: "Lecture Slide",
-        link: "#",
+        link: "https://drive.google.com/file/d/1oDDoiWZp3yiiiBWO7HQouHUyhwsJI_NU/view?usp=sharing",
         labelInfo: "Slide",
       },
       {
         type: "video", // optional - just video has tag
-        text: "Introduction to course, what is course?",
-        link: "#",
+        text: "Introduction",
+        link: "https://drive.google.com/file/d/1oDDoiWZp3yiiiBWO7HQouHUyhwsJI_NU/view?usp=sharing",
         labelInfo: "01:09:51",
       },
       {
         type: "video",
-        text: "Course part 1",
-        link: "#",
-        labelInfo: "01:22:58",
-      },
-      {
-        type: "video",
-        text: "Course part 2",
-        link: "#",
-        labelInfo: "01:19:41",
+        text: "Nerual network history",
+        link: "https://drive.google.com/file/d/1AawWJvSBv6nAby5QG7KF0g9ghFZte36n/view?usp=sharing",
+        labelInfo: "00:20:09",
       },
     ],
   },
-  {
-    text: "Cloud Reference Architecture",
-    startText:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-    sections: [
-      {
-        text: "Lecture Slide",
-        link: "#",
-        labelInfo: "Slide",
-      },
-      {
-        text: "Course part link",
-        link: "#",
-        labelInfo: "Paper",
-        items: [
-          { label: "Paper 1", link: "#" },
-          { label: "Paper 2", link: "#" },
-          { label: "Paper 3", link: "#" },
-        ],
-      },
-    ],
-  },
+  // {
+  //   text: "Introduction to Course",
+  //   requirements: "You and your motivation.", // optional
+  //   startText: "Lets start to know what is the course and how it improve...", // optional
+  //   sections: [
+  //     {
+  //       text: "Lecture Slide",
+  //       link: "#",
+  //       labelInfo: "Slide",
+  //     },
+  //     {
+  //       type: "video", // optional - just video has tag
+  //       text: "Introduction to course, what is course?",
+  //       link: "#",
+  //       labelInfo: "01:09:51",
+  //     },
+  //     {
+  //       type: "video",
+  //       text: "Course part 1",
+  //       link: "#",
+  //       labelInfo: "01:22:58",
+  //     },
+  //     {
+  //       type: "video",
+  //       text: "Course part 2",
+  //       link: "#",
+  //       labelInfo: "01:19:41",
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "Cloud Reference Architecture",
+  //   startText:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+  //   sections: [
+  //     {
+  //       text: "Lecture Slide",
+  //       link: "#",
+  //       labelInfo: "Slide",
+  //     },
+  //     {
+  //       text: "Course part link",
+  //       link: "#",
+  //       labelInfo: "Paper",
+  //       items: [
+  //         { label: "Paper 1", link: "#" },
+  //         { label: "Paper 2", link: "#" },
+  //         { label: "Paper 3", link: "#" },
+  //       ],
+  //     },
+  //   ],
+  // },
 
-  {
-    text: "Your project section",
-    tag: "Project",
-    requirements: "Project requirements",
-    startText: "Project start text.",
-    sections: [
-      {
-        text: "Download the IntelliJ IDE",
-        labelInfo: "IDE",
-      },
-      {
-        type: "video",
-        text: "How to set up environment",
-        link: "#",
-        labelInfo: "Preparing",
-        items: [{ label: "project Libraries", link: "#" }],
-      },
-      {
-        text: "Project Document",
-        link: "#",
-        labelInfo: "Document",
-        items: [
-          { label: "Deadline 20th Feb" },
-          { label: "Submit on github", link: "#" },
-        ],
-      },
-    ],
-  },
+  // {
+  //   text: "Your project section",
+  //   tag: "Project",
+  //   requirements: "Project requirements",
+  //   startText: "Project start text.",
+  //   sections: [
+  //     {
+  //       text: "Download the IntelliJ IDE",
+  //       labelInfo: "IDE",
+  //     },
+  //     {
+  //       type: "video",
+  //       text: "How to set up environment",
+  //       link: "#",
+  //       labelInfo: "Preparing",
+  //       items: [{ label: "project Libraries", link: "#" }],
+  //     },
+  //     {
+  //       text: "Project Document",
+  //       link: "#",
+  //       labelInfo: "Document",
+  //       items: [
+  //         { label: "Deadline 20th Feb" },
+  //         { label: "Submit on github", link: "#" },
+  //       ],
+  //     },
+  //   ],
+  // },
 ];
