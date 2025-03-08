e979d788d2d71c2db08289fc36e83c12069389d3
Mohsen Gholami <imohsen2002@gmail.com>
2025-03-08 15:03:22 +0330

    Update team.js

--- data/team.js
+++ data/team.js
@@ -1,17 +1,17 @@
 export const team = [
   {
-    name: "Dr <Professor Name>",
-    profile: "./avatar.jpg",
+    name: "Dr Mojataba Rouhani",
+    profile: "./dr-profile.jpg",
     role: "Assistant Professor At FUM",
     socials: [
-      { social: "Mail", link: "#" },
-      { social: "Telegram", link: "#" },
-      { social: "website", link: "#" },
+      { social: "Mail", link: "rouhani@um.ac.ir" },
+      { social: "Telegram", link: "t.me/M0_rouh" },
+      { social: "website", link: "prof.um.ac.ir/rouhani" },
     ],
   },
   {
     name: "Mohsen Gholami Golkhatmi",
-    role: "Teacher Assistant & Creator",
+    role: "Teacher Assistant",
     profile: "./prof-mohsen.jpg",
     socials: [
       { social: "G-mail", link: "mailto:iMohsen2002@gmail.com" },
